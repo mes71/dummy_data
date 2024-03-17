8879471d4643e7d4019d8e7da4348a9eb8d43637
Mr.Mojtaba <mr.mojtaba47@gamil.com>
2024-03-17 14:54:46 +0330

    feat => add first logic for word generator

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,7 +1,21 @@
 library dummy_data;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:dummy_data/data/constants.dart';
+
+class Dummy {
+  Dummy._();
+
+  late int word;
+
+  static String getWord(int word) {
+    StringBuffer buffer = StringBuffer();
+    for (int i = 0; i < word; i++) {
+      buffer.write(wordEn.elementAt(i));
+      if(i < word - 1){
+        buffer.write(" ");
+      }
+
+    }
+    return buffer.toString();
+  }
 }
--- test/dummy_data_test.dart
+++ test/dummy_data_test.dart
@@ -1,12 +1,24 @@
+import 'package:dummy_data/data/constants.dart';
+import 'package:dummy_data/dummy_data.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dummy_data/dummy_data.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group(" Word generator Test", () {
+    test('Test getWord method', () {
+      int numberOfWords = 150;
+      String result = Dummy.getWord(numberOfWords);
+      expect(result.split(" ").length, numberOfWords);
+    });
+
+    test('Test getWord method', () {
+      // Arrange
+      int numberOfWords = 5;
+      String expectedWords =
+          List.from(wordEn).sublist(0, numberOfWords).join(" ");
+      // Act
+      String result = Dummy.getWord(numberOfWords);
+      // Assert
+      expect(result, expectedWords);
+    });
   });
 }
